812b1a4597f9d9774b37ef2a3ce800177b9df92f
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2021-11-03 11:42:56 +0100

    print userPage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { BiUserCircle, BiSearch, BiHome, BiGroup } from 'react-icons/bi';
+import UserPage from './UserPage';
+
 
 function Navbar() {
     // Tar bort denna styling sen
@@ -42,7 +44,7 @@ function Navbar() {
                 </Route>
                 <Route path='/friends'>VÄNNER</Route>
                 <Route path='/recipes'>SLUMPADE RECEPT</Route>
-                <Route path='/mypage'>MIN SIDA</Route>
+                <Route path='/mypage'><UserPage /*user={currentUser}*//></Route> 
                 <Route path='*' component='' />{' '}
                 {/* Här kan vi lägga till en 404-sida sen */}
             </Switch>
