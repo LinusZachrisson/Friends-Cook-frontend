ff6616e75d4d949531e6ea70d8ef0eb59bd8bb66
johannesfjellborg <johannesfjellborg@hotmail.com>
2021-11-04 09:37:26 +0100

    Get 10 random recipes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { BiUserCircle, BiSearch, BiHome, BiGroup } from 'react-icons/bi';
-import Friends from './Friends';
+import React from "react";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BiUserCircle, BiSearch, BiHome, BiGroup } from "react-icons/bi";
+import Friends from "./Friends";
+import RandomRecipes from "./RandomRecipes";
 
 function Navbar() {
-    // Tar bort denna styling sen
-    const style = {
-        display: 'flex',
-        justifyContent: 'center',
-        margin: '20px',
-        fontSize: '25px',
-    };
+  // Tar bort denna styling sen
+  const style = {
+    display: "flex",
+    justifyContent: "center",
+    margin: "20px",
+    fontSize: "25px",
+  };
 
-    return (
-        <Router>
-            <nav style={style}>
-                <Link to='/'>
-                    <div>
-                        <BiHome />
-                    </div>
-                </Link>
-                <Link to='/friends'>
-                    <div>
-                        <BiGroup />
-                    </div>
-                </Link>
-                <Link to='/recipes'>
-                    <div>
-                        <BiSearch />
-                    </div>
-                </Link>
-                <Link to='/mypage'>
-                    <div>
-                        <BiUserCircle />
-                    </div>
-                </Link>
-            </nav>
-            <Switch>
-                <Route exact path='/'>
-                    START
-                </Route>
-                <Route path='/friends' component={Friends} />
-                <Route path='/recipes'>SLUMPADE RECEPT</Route>
-                <Route path='/mypage'>MIN SIDA</Route>
-                <Route path='*' component='' />{' '}
-                {/* Här kan vi lägga till en 404-sida sen */}
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <nav style={style}>
+        <Link to="/">
+          <div>
+            <BiHome />
+          </div>
+        </Link>
+        <Link to="/friends">
+          <div>
+            <BiGroup />
+          </div>
+        </Link>
+        <Link to="/recipes">
+          <div>
+            <BiSearch />
+          </div>
+        </Link>
+        <Link to="/mypage">
+          <div>
+            <BiUserCircle />
+          </div>
+        </Link>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          START
+        </Route>
+        <Route path="/friends" component={Friends} />
+        <Route path="/recipes" component={RandomRecipes} />
+        <Route path="/mypage">MIN SIDA</Route>
+        <Route path="*" component="" />{" "}
+        {/* Här kan vi lägga till en 404-sida sen */}
+      </Switch>
+    </Router>
+  );
 }
 
 export default Navbar;
