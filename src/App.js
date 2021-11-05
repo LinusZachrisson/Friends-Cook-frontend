e59a5646386445a5c244f4e60a766f3a98c196cb
Mathilda Pettersson <mathilda.pettersson@medieinstitutet.se>
2021-11-05 11:44:04 +0100

    changed mongo client to mongoose

--- src/App.js
+++ src/App.js
@@ -1,65 +1,67 @@
-import "./App.css";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import RegisterUser from "./components/RegisterUser";
-import UserContext from "./UserContext";
-import axios from "axios";
-import LoginUser from "./components/LoginUser";
-import Navbar from "./components/Navbar";
+import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import RegisterUser from './components/RegisterUser';
+import UserContext from './UserContext';
+import axios from 'axios';
+import LoginUser from './components/LoginUser';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [username, setUsername] = useState();
+    const [username, setUsername] = useState();
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:4000/user/islogedin", {
-        withCredentials: true,
-      })
-      .then((res) => {
-        setUsername(res.data.username);
-      });
-  }, []);
+    useEffect(() => {
+        axios
+            .get('http://localhost:4000/user/islogedin', {
+                withCredentials: true,
+            })
+            .then((res) => {
+                setUsername(res.data.username);
+            });
+    }, []);
 
-  const logOut = () => {
-    axios
-      .post("http://localhost:4000/user/logout", {}, { withCredentials: true })
-      .then(() => setUsername());
-  };
+    const logOut = () => {
+        axios
+            .post(
+                'http://localhost:4000/user/logout',
+                {},
+                { withCredentials: true }
+            )
+            .then(() => setUsername());
+    };
 
-  return (
-    <UserContext.Provider value={{ username: username, setUsername }}>
-      {username !== undefined ? (
-        <div>
-          <div>
-            {" "}
-            Du är nu inloggad som {username}{" "}
-            {/* logga ut knapp i komponent för inloggade användare */}
-            <button onClick={logOut}>Logga ut</button>
-          </div>
-          <Navbar />
-        </div>
-      ) : (
-        <BrowserRouter>
-          <div className="App">Friends & Cook</div>
-
-          <div>
-            {/* Komponent för inloggade användare */}
-
-            {!username && <div>Inte inloggad</div>}
-          </div>
-          <div>
-            <Link to={"/Register"}>Registrera ny användare</Link>
-          </div>
-          <Switch>
-            <Route exact path="/">
-              <LoginUser />
-            </Route>
-            <Route exact path={"/Register"} component={RegisterUser} />
-          </Switch>
-        </BrowserRouter>
-      )}
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={{ username: username, setUsername }}>
+            {username !== undefined ? (
+                <div>
+                    <div>
+                        {' '}
+                        Du är nu inloggad som {username}{' '}
+                        {/* logga ut knapp i komponent för inloggade användare */}
+                        <button onClick={logOut}>Logga ut</button>
+                    </div>
+                    <Navbar user={username} />
+                </div>
+            ) : (
+                <BrowserRouter>
+                    <div className='App'>Friends & Cook</div>
+                    <div>
+                        <Link to={'/Register'}>Registrera ny användare</Link>
+                    </div>
+                    <Switch>
+                        <Route exact path='/'>
+                            <LoginUser />
+                        </Route>
+                        <Route
+                            exact
+                            path={'/Register'}
+                            component={RegisterUser}
+                        />
+                    </Switch>
+                </BrowserRouter>
+            )}
+        </UserContext.Provider>
+    );
 }
 
 export default App;
--- src/components/LikedRecipes.js
+++ src/components/LikedRecipes.js
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 function LikedRecipes() {
-  let [likedRecipes, setLikedRecipes] = useState([]);
+    let [likedRecipes, setLikedRecipes] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:4000/users/getlikedrecipes")
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
-        setLikedRecipes(data);
-      });
-  }, []);
+    useEffect(() => {
+        fetch('http://localhost:4000/users/getlikedrecipes')
+            .then((resp) => resp.json())
+            .then((data) => {
+                setLikedRecipes(data);
+            });
+    }, []);
 
-  return (
-    <div>
-      {likedRecipes.map((recipe) => (
-        <div key={recipe._id}>
-          <img src={recipe.ImageUrl} /> {recipe.Title} {recipe.LikedBy}
-          {/* På liked by loopar man igenom arrayn och tar fram user */}
+    return (
+        <div>
+            {likedRecipes.map((recipe) => (
+                <div key={recipe._id}>
+                    <img src={recipe.ImageUrl} /> {recipe.Title}{' '}
+                    {recipe.LikedBy}
+                    {/* På liked by loopar man igenom arrayn och tar fram user */}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default LikedRecipes;
