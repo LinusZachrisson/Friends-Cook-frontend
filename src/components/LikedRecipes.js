0870749378732ae56b3513f8d61fc7c64dcc7b3d
dmlfahfn <dmlfahfn1@hotmail.se>
2021-11-08 21:57:18 +0100

    cors issue

--- src/components/LikedRecipes.js
+++ src/components/LikedRecipes.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
+
 function LikedRecipes() {
     let [likedRecipes, setLikedRecipes] = useState([]);
 
@@ -12,12 +13,30 @@ function LikedRecipes() {
             });
     }, []);
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(e.target.id);
+
+        fetch('https://www.ica.se/recept/'  + e.target.id)
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data);
+            });
+    };
+
+    // https://www.ica.se/recept/717330
+
     return (
         <div>
             {likedRecipes.map((recipe) => (
-                <div key={recipe._id}>
+                <div key={recipe.Id}>
                     <img src={recipe.ImageUrl} /> 
-                    <div>{recipe.Title}</div>
+                    <div
+                    id={recipe.Id} 
+                    onClick={handleClick}
+                    >
+                    {recipe.Title}
+                </div>
                     <div>{recipe.LikedBy.length - 1 === 0 ? <div>{recipe.LikedBy[0]} gillar receptet. </div> : <div> {recipe.LikedBy[0]} och {recipe.LikedBy.length - 1} vänner till gillar receptet.</div>}  </div>
                 </div>
             ))}
