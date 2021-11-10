efde68f77d5daf0f729d85bb4e1a1ec869f95c27
unknown <zlinus.zachrison@gmail.com>
2021-11-10 13:32:11 +0100

    finishing like on start page

--- src/components/LikedRecipes.js
+++ src/components/LikedRecipes.js
@@ -1,51 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from "react";
+import UserContext from "../UserContext";
 
-function LikedRecipes() {
-    let [likedRecipes, setLikedRecipes] = useState([]);
+function LikedRecipes(prop) {
+  let [likedRecipes, setLikedRecipes] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:4000/users/getlikedrecipes')
-            .then((resp) => resp.json())
-            .then((data) => {
-                console.log(data);
-                setLikedRecipes(data);
-            });
-    }, []);
+  const user = useContext(UserContext);
 
-    return (
-        <div className='flow-container'>
-            <p className='flow-p'>
-                <span>Behöver du middagstips?</span>
-                <br /> Kolla in vad dina vänner gillar för recept.
-            </p>
-            {likedRecipes.map((recipe) => (
-                <div key={recipe._id} className='flow-recipe-container'>
-                    <a href={'https://www.ica.se/recept/' + recipe.Id}>
-                        <img
-                            src={recipe.ImageUrl}
-                            alt='Bilden kunde inte laddas'
-                        />
-                    </a>
-                    <p className='flow-recipe-title'>{recipe.Title}</p>{' '}
-                    <div className='flow-likedby'>
-                        {recipe.LikedBy.length - 1 === 0 ? (
-                            <p>
-                                Gillas av <span>{recipe.LikedBy[0]}</span>{' '}
-                            </p>
-                        ) : (
-                            <p>
-                                {' '}
-                                <span>{recipe.LikedBy[0]}</span> och{' '}
-                                <span>{recipe.LikedBy.length - 1}</span> vänner
-                                till gillar receptet.
-                            </p>
-                        )}
-                    </div>
-                    {/* <hr /> */}
-                </div>
-            ))}
+  useEffect(() => {
+    fetch("http://localhost:4000/users/getlikedrecipes")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setLikedRecipes(data);
+      });
+  }, []);
+
+  const handleClick = (evt) => {
+    console.log(evt.target.id);
+    console.log(evt.target.parentNode.id);
+    console.log(evt.target.title);
+    console.log(user.username);
+    evt.target.textContent = "Gillat!";
+    const resp = fetch("http://localhost:4000/write", {
+      method: "POST",
+      body: JSON.stringify({
+        Id: evt.target.id,
+        Title: evt.target.title,
+        ImageUrl: evt.target.parentNode.id,
+        LikedBy: user.username,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
+  return (
+    <div className="flow-container">
+      <p className="flow-p">
+        <span>Behöver du middagstips?</span>
+        <br /> Kolla in vad dina vänner gillar för recept.
+      </p>
+      {likedRecipes.map((recipe) => (
+        <div
+          key={recipe._id}
+          className="flow-recipe-container"
+          id={recipe.ImageUrl}
+        >
+          <a href={"https://www.ica.se/recept/" + recipe.Id}>
+            <img src={recipe.ImageUrl} alt="Bilden kunde inte laddas" />
+          </a>
+          <p className="flow-recipe-title">{recipe.Title}</p>{" "}
+          <div className="flow-likedby">
+            {recipe.LikedBy.length - 1 === 0 ? (
+              <p>
+                Gillas av <span>{recipe.LikedBy[0]}</span>{" "}
+              </p>
+            ) : (
+              <p>
+                {" "}
+                <span>{recipe.LikedBy[0]}</span> och{" "}
+                <span>{recipe.LikedBy.length - 1}</span> vänner till gillar
+                receptet.
+              </p>
+            )}
+          </div>
+          <button
+            onClick={handleClick}
+            title={recipe.Title}
+            id={recipe.Id}
+            imageurl={recipe.ImageUrl}
+          >
+            Gilla
+          </button>
+          {/* <hr /> */}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default LikedRecipes;
--- src/components/RandomRecipes.js
+++ src/components/RandomRecipes.js
@@ -1,83 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const RandomRecipes = (prop) => {
-    const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => {
-        fetchRecipes();
-    }, []);
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
 
-    const fetchRecipes = () => {
-        axios
-            .get('http://localhost:4000/api/')
-            .then((res) => {
-                console.log(res.data.Recipes);
-                setRecipes(res.data.Recipes);
-            })
-            .catch((err) => console.log(err));
-    };
+  const fetchRecipes = () => {
+    axios
+      .get("http://localhost:4000/api/")
+      .then((res) => {
+        console.log(res.data.Recipes);
+        setRecipes(res.data.Recipes);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const handleClick = (evt) => {
-        console.log(evt.target.id);
-        console.log(evt.target.parentNode.id);
-        console.log(evt.target.title);
-        evt.target.textContent = 'Gillat!';
-        const resp = fetch('http://localhost:4000/write', {
-            method: 'POST',
-            body: JSON.stringify({
-                Id: evt.target.id,
-                Title: evt.target.title,
-                ImageUrl: evt.target.parentNode.id,
-                LikedBy: prop.user,
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-    };
+  const handleClick = (evt) => {
+    console.log(evt.target.id);
+    console.log(evt.target.parentNode.id);
+    console.log(evt.target.title);
+    evt.target.textContent = "Gillat!";
+    const resp = fetch("http://localhost:4000/write", {
+      method: "POST",
+      body: JSON.stringify({
+        Id: evt.target.id,
+        Title: evt.target.title,
+        ImageUrl: evt.target.parentNode.id,
+        LikedBy: prop.user,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
 
-    return (
-        <div className='random-recipes-container'>
-            <p className='random-first-text'>
-                Här du hitta nya recept att <span>gilla, laga</span>
-                <br /> och <span>inspirera</span> dina vänner med.
-            </p>
-            <a href='/recipes' className='new-recipes-btn'>
-                Slumpa nya recept
+  return (
+    <div className="random-recipes-container">
+      <p className="random-first-text">
+        Här du hitta nya recept att <span>gilla, laga</span>
+        <br /> och <span>inspirera</span> dina vänner med.
+      </p>
+      <a href="/recipes" className="new-recipes-btn">
+        Slumpa nya recept
+      </a>
+      <div>
+        {recipes.map((recipe) => (
+          <div
+            key={recipe.Id}
+            id={recipe.ImageUrl}
+            className="random-recipe-box"
+          >
+            <a
+              href={"https://www.ica.se/recept/" + recipe.Id}
+              className="random-recipe-container"
+            >
+              <img src={recipe.ImageUrl} alt="Bilden kunde inte laddas" />
+              <p className="random-recipe-title">{recipe.Title}</p>
             </a>
-            <div>
-                {recipes.map((recipe) => (
-                    <div
-                        key={recipe.Id}
-                        id={recipe.ImageUrl}
-                        className='random-recipe-box'
-                    >
-                        <a
-                            href={'https://www.ica.se/recept/' + recipe.Id}
-                            className='random-recipe-container'
-                        >
-                            <img
-                                src={recipe.ImageUrl}
-                                alt='Bilden kunde inte laddas'
-                            />
-                            <p className='random-recipe-title'>
-                                {recipe.Title}
-                            </p>
-                        </a>
-                        <button
-                            onClick={handleClick}
-                            title={recipe.Title}
-                            id={recipe.Id}
-                            imageurl={recipe.ImageUrl}
-                        >
-                            Gilla
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+            <button
+              onClick={handleClick}
+              title={recipe.Title}
+              id={recipe.Id}
+              imageurl={recipe.ImageUrl}
+            >
+              Gilla
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RandomRecipes;
