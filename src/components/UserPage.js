3837c805740fc13f5ead888371fe0f7c174e1991
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2021-11-10 18:43:45 +0100

    Kan ogilla / gilla recept

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { BiTrash } from 'react-icons/bi';
+
 
 const UserPage = (prop) => {
     const [likedRecepies, setLikedRecepies] = useState([]);
-    console.log(prop);
+
     useEffect(() => {
         fetch(`http://localhost:4000/readall`)
             .then((res) => res.json())
@@ -14,10 +14,28 @@ const UserPage = (prop) => {
                         recepies.push(data[i]);
                     }
                 }
-                console.log(recepies);
                 setLikedRecepies(recepies);
             });
-    }, []);
+    }, [likedRecepies]);
+
+    const handleOnClick = (isRemoved, evt) => {
+        console.log(evt.target.id);
+        console.log(evt.target.parentNode.id);
+        console.log(evt.target.title);
+        const resp = fetch("http://localhost:4000/write", {
+        method: "POST",
+        body: JSON.stringify({
+            Id: evt.target.id,
+            Title: evt.target.title,
+            ImageUrl: evt.target.parentNode.id,
+            LikedBy: prop.myLikes,
+            Remove: isRemoved,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+        }); 
+    }
 
     return (
         <div className='profile-container'>
@@ -27,9 +45,14 @@ const UserPage = (prop) => {
             <div className='profile-liked-rec-container'>
                 {likedRecepies.map((food, index) => {
                     return (
-                        <div key={index} className='profile-recipe-con'>
+                        <div id={food.ImageUrl} key={index} className='profile-recipe-con'>
                             <img src={food.ImageUrl} alt='Food of recepie' />{' '}
                             <h4>{food.Title}</h4>
+                            <p>{food.LikedBy}</p>
+                            {food.LikedBy.includes(prop.myLikes) ? 
+                            <button title={food.Title} id={food.Id} imageurl={food.ImageUrl}onClick={(evt) => handleOnClick(true, evt)}>Unlike</button> : 
+                            <button title={food.Title} id={food.Id} imageurl={food.ImageUrl} onClick={(evt) => handleOnClick(false, evt)}> Like </button>}
+                            
                         </div>
                     );
                 })}
